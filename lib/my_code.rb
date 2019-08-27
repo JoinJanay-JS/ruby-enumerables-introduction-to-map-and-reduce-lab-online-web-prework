7148208eabe32cb5b9af750d52094ba59404997f
Janay Bailey <tjanay87@gmail.com>
2019-08-27 16:23:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,5 +4,6 @@ def map_to_no_change(1[1,2,3])
 def map_to_double(2[1,2,3])  
 def map_to_square(.sqrt[1,2,3])
   
-def reduce_to_total()
-def 
+def reduce_to_total([])
+def reduce_to_all_true([])
+def reduce_to_any_true([])
